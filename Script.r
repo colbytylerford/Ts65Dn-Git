a75c1550bef329cd9e352ba9b6e6ff7100915ea2
Colby Ford <colbytylerford@gmail.com>
2016-12-01 16:37:48 -0500

    Remove ShinyApps.io Connection Credentials

--- Script.r
+++ Script.r
@@ -8,7 +8,4 @@ library(shinydashboard)
 
 runApp("Ts65DnApp")
 
-#rsconnect::setAccountInfo(name='colbytylerford', token='324E96B5C29C12415746640C89856527', secret='m6yGX8HHBHTHJcqDJcPjqBPRFVsighQiLDwQxTx3')
-#library(rsconnect)
-#rsconnect::deployApp("Ts65DnApp")
 #runApp("Ts65DnApp",display.mode = "showcase")
